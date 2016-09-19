a88e54aec73b0da3063f36c090513b7def50a97b
rmellema <rene.mellema@gmail.com>
2016-09-19 14:41:23 +0200

    Added a Claim class that agents can use to claim a room at a specific time. Can only attack other claims

---
+++ argument/claim.py
@@ -0,0 +1,24 @@
+from argument import Argument
+
+class Claim(Argument):
+    def __init__(self, framework, room, time, name = None):
+        super(Claim, self).__init__(framework, name)
+        self._room = room
+        self._time = time
+
+    def get_room(self):
+        return self._room
+
+    def get_time(self):
+        return self._time
+
+    def can_attack(self, other):
+        return (isinstance(other, Claim) and 
+                self.get_room() == other.get_room() and
+                self.get_time() == other.get_time())
+
+    def can_undercut(self, other):
+        return False
+
+    def can_support(self, other):
+        return False
---
+++ tests/test_argument/test_claim.py
@@ -0,0 +1,25 @@
+import unittest
+from argument.argumentationframework import ArgumentationFramework
+from argument.claim import Claim
+
+class ClaimTest(unittest.TestCase):
+    def setUp(self):
+        self._fw = ArgumentationFramework()
+
+    def test_can_attack(self):
+        c1 = Claim(self._fw, 1, "11:00 - 13:00")
+        c2 = Claim(self._fw, 1, "11:00 - 13:00")
+        c3 = Claim(self._fw, 2, "11:00 - 13:00")
+        c4 = Claim(self._fw, 1, "13:00 - 15:00")
+        self.assertTrue(c1.can_attack(c2), 
+                "Claims can attack other claims if they claim the same room/time")
+        self.assertFalse(c1.can_attack(c3))
+        self.assertFalse(c1.can_attack(c4))
+
+    def test_get_room(self):
+        c = Claim(self._fw, 1, "11:00 - 13:00")
+        self.assertEqual(c.get_room(), 1)
+
+    def test_get_time(self):
+        c = Claim(self._fw, 1, "11:00 - 13:00")
+        self.assertEqual(c.get_time(), "11:00 - 13:00")
