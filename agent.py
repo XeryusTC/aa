fd7da68e43e80d00875cb6693c1c4b246a326eca
XeryusTC <armadillo@onenetbeyond.org>
2016-09-27 16:11:08 +0200

    Fix agent file

--- agent.py
+++ agent.py
@@ -2,13 +2,11 @@ import yaml
 class Agent(object):
     def __init__(self, name, courses = []):
         self._name = name
-        #self.courses = courses
-
-
+        self._courses = courses
 
     def __str__(self):
-       return '{} | course: {} | students:{}'.format(self.name, self.course, self.students)
-    
+       return '{} | course: {}'.format(self._name, self._courses)
+
     def get_name(self):
         return self._name
 
@@ -20,12 +18,12 @@ class Agent(object):
     #def get_students(self):
     #    return self.students
 
-#def load_agents(filename):
-#    with open(filename, 'r') as f:
-#	    agent_doc = yaml.load(f)
+def load_agents(filename):
+    with open(filename, 'r') as f:
+        agent_doc = yaml.load(f)
 
-#   teacher_list = []
-#   for teacher in agent_doc['teachers']:
-#	    r = Agent(teacher['name'], teacher['course'], teacher['students'])
-#	    teacher_list.append(r)
-#   return teacher_list
+    teacher_list = []
+    for teacher in agent_doc['teachers']:
+        r = Agent(teacher['name'], teacher['courses'])
+        teacher_list.append(r)
+    return teacher_list
