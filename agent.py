0b3cc163453a5215bc058cb420914f8b39cd6961
rmellema <rene.mellema@gmail.com>
2016-09-28 14:30:41 +0200

    Implemented make_counter

--- agent.py
+++ agent.py
@@ -1,4 +1,7 @@
 import yaml
+from argument.sizeargument import SizeArgument
+from argument.argument import Counter
+
 class Agent(object):
     def __init__(self, name, courses = []):
         self.name = name
@@ -7,7 +10,39 @@ class Agent(object):
     def __str__(self):
        return '#<{} | course: {} >'.format(self.name, self.courses)
 
-    def make_counter(self, fw):
+    def make_counter(self, fw, room):
+        grounded_others = [arg for arg in fw.get_grounded()
+                            if arg.owner != self]
+        if len(grounded_others) != 0:
+            for arg in grounded_others:
+                att = self.construct_attack(fw, room, arg)
+                if att:
+                    return att
+        else:
+            own_ungrounded = [arg for arg in fw.get_arguments()
+                                if arg.owner == self
+                                if not fw.is_grounded(arg)]
+            for arg in own_ungrounded:
+                for att, _ in fw.get_attacks(argument = arg):
+                    counter = self.construct_attack(fw,room, att)
+                    if counter:
+                        return counter
+        return None
+
+    def construct_attack(self, fw, room, arg):
+        own_arguments = [arg for arg in fw.get_arguments()
+                            if arg.owner == self]
+        other = arg.owner.active_course
+        if self.active_course.students >= other.students:
+            sizearg = [a for a in own_arguments
+                        if isinstance(a, SizeArgument)]
+            if len(sizearg) != 0:
+                if fw.is_grounded(sizearg[0]):
+                    return Counter("attack", sizearg[0], arg)
+            else:
+                return Counter("attack", 
+                        SizeArgument(fw, self, room, self.active_course.students),
+                        arg)
         return None
 
 class Course:
